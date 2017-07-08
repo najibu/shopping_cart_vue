b2a9f8dd00345d44d89522488e6ecf8132055613
najibu <ns.najibu@gmail.com>
2017-07-08 16:51:19 +0100

    Added shopping cart summary

---
+++ src/components/shop/shopping-cart/ShoppingCartSummary.vue
@@ -0,0 +1,52 @@
+<template>
+  <ul class="list-group">
+    <li class="list-group-item">
+      Subtotal ({{ itemsQauntity }} {{ 'item' | pluralize(itemsQuantity) }})
+      <em v-if="productDiscount"><b>(30% OFF applied)</b></em>
+    </li>
+
+    <li class="list-group-item">
+      Shipping:
+      <span v-if="!freeShipping">{{ shipping | formatMoney }}</span>
+      <em v-if="freeShipping">Free Shipping</em>
+    </li>
+
+    <li class="list-group-item">
+      Taxes: {{taxes | formatMoney }}
+    </li>
+
+    <li class="list-group-item">
+      <strong>Total:</strong>
+      <strong v-if="!totalDiscount">{{ total | formatMoney }}</strong>
+      <span v-if="totalDiscount">
+        <strike>{{ total + 100 | formatMoney }}</strike>
+        <strong>{{ totalWithDiscount | formatMoney }}</strong>
+      </span>
+    </li>
+  </ul>
+</template>
+
+<script>
+import { mapGetters, mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters([
+      'itemsQuantity',
+      'subtotal',
+      'taxes',
+      'shipping',
+      'total'
+    ]),
+    ...mapState({
+      productDiscount: state => state.shoppingCart.productDiscount,
+      freeShipping: state => state.shoppingCart.freeShipping,
+      totalDiscount: state => state.shoppingCart.totalDiscount
+    }),
+    totalWithDiscount () {
+      let total = this.$store.getters.total
+      return total > 0 ? this.total : 0
+    }
+  }
+}
+</script>
