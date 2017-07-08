988241ac911db4534171d4d5ccfaca823da64b32
najibu <ns.najibu@gmail.com>
2017-07-08 16:57:17 +0100

    Added coupons

---
+++ src/components/shared/Toggle.vue
@@ -0,0 +1,16 @@
+<template>
+  <label class="custom-control custom-checkbox">
+    <input type="checkbox" class="custom-control-input" @change="action(data)">
+    <span class="custom-control-indicator"></span>
+    <span class="custom-control-description">{{data.title}}</span>
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    data: Object,
+    action: Function
+  }
+}
+</script>
--- src/components/shop/Coupons.vue
+++ src/components/shop/Coupons.vue
@@ -1,7 +1,33 @@
 <template>
-  
+  <div class="mt-2">
+    <h3>Promotions</h3>
+
+    <toggle
+      :data="coupon"
+      :action="toggleCoupon"
+      v-for="coupon in promotions"
+      :key="coupon.id"
+    >
+      
+    </toggle>
+  </div>
 </template>
 
 <script>
-  
+import Toggle from './../shared/Toggle'
+import { mapActions, mapState } from 'vuex'
+
+export default {
+  computed: mapState({
+    promotions: state => state.promotions.all
+  }),
+  methods: mapActions([
+    'getPromotions',
+    'toggleCoupon'
+  ]),
+  components: { Toggle },
+  created () {
+    this.getPromotions()
+  }
+}
 </script>
